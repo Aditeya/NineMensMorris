358a44c3b540404129ec0635c66942e8fc9a687e
EltoCode <ef356@live.mdx.ac.uk>
2021-02-17 22:04:18 +0400

    Added Internal Game Logic Class 'NnMnMrrs' \n Added Internal Game Logic Class Demo 'NMMDemo'

---
+++ src/ninemensmorris/NMMLogicDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ninemensmorris;
+
+/**
+ * Used to demo function of the NMM internal logic class
+ * @author eltojaro
+ */
+public class NMMLogicDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        while(true)
+        {
+            System.out.println("Oh hi bro");
+        }
+        
+    }
+    
+}
---
+++ src/ninemensmorris/NnMnMrrs.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ninemensmorris;
+
+/**
+ * The class whose objects will implement the boards of Nine Mens Morris
+ * Handles the internal logic of NMM
+ * @author eltojaro
+ */
+public class NnMnMrrs {
+    
+    
+    
+}
