11ad5f4a730706aafe33b2e7b661442d00c1c4fb
EltoCode <ef356@live.mdx.ac.uk>
2021-02-17 23:18:50 +0400

    Added a Coin Type Enum

---
+++ src/ninemensmorris/MCoinType.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ninemensmorris;
+
+/**
+ * An Enum for the coin type
+ * Holds Empty, Black or White
+ * @author eltojaro
+ */
+public enum MCoinType {
+    EMPTY,
+    BLACK,   //You have no idea how hard I had to fight the urge to call this ni-
+    WHITE
+    
+}
