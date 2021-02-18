2cb8f6d3a3a6e57988ad8bcca039ef38cec10a82
EltoCode <ef356@live.mdx.ac.uk>
2021-02-18 22:48:31 +0400

    Moved enum to own package

--- src/ninemensmorris/enums/MCoinType.java
+++ src/ninemensmorris/enums/MCoinType.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ninemensmorris;
+package ninemensmorris.enums;
 
 /**
  * An Enum for the coin type
@@ -16,3 +16,4 @@ public enum MCoinType {
     WHITE
     
 }
+
---
+++ src/ninemensmorris/enums/PrintType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ninemensmorris.enums;
+
+/**
+ * An Enum for the print type
+ * Holds Value or location
+ * @author eltojaro
+ */
+public enum printType {
+    VALUE,
+    LOC
+}
