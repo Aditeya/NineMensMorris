380cd6148d6ef16c87f81d417328b8afb6d9ef59
aditeya <22963960+Aditeya@users.noreply.github.com>
2021-02-19 16:47:55 +0400

    Added Better Fix for File and Formatted Code

--- src/ninemensmorris/NMMLogicDemo.java
+++ src/ninemensmorris/NMMLogicDemo.java
@@ -9,6 +9,7 @@ import ninemensmorris.enums.PrintType;
 
 /**
  * Used to demo function of the NMM internal logic class
+ *
  * @author eltojaro
  */
 public class NMMLogicDemo {
@@ -17,28 +18,22 @@ public class NMMLogicDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        int[] test  = {5, 3, 2, 1};
+
+        int[] test = {5, 3, 2, 1};
         test2(test);
         System.out.println(test[1]);
-        
+
         NnMnMrrs nmm = new NnMnMrrs();
-        
+
         nmm.cmdPrint(PrintType.LOC);
         nmm.cmdPrint(PrintType.RAW_LOC);
         nmm.cmdPrint(PrintType.RAW_VALUE);
         nmm.cmdPrint(PrintType.VALUE);
-        
-        while(true)
-        {
-            System.out.println("Oh hi bro");
-        }
-        
+
     }
-    
-    public static void test2(int[] test)
-    {
+
+    public static void test2(int[] test) {
         test[1] = 19;
     }
-    
+
 }
