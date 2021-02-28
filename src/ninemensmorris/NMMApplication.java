10cb45c11c14d1ec5ed81605de3f945d65609794
aditeya <22963960+Aditeya@users.noreply.github.com>
2021-02-28 20:55:30 +0400

    Formatted Code in NMMApplication

--- src/ninemensmorris/NMMApplication.java
+++ src/ninemensmorris/NMMApplication.java
@@ -31,59 +31,62 @@ import javafx.stage.Stage;
  */
 public class NMMApplication extends Application {
 
-   
     private char marker;
     private boolean myTurn = false;
     private char whoseTurn = 'X';
-            int row_colnum = 5;
+    int row_colnum = 5;
 
     private Cell[][] cell = new Cell[row_colnum][row_colnum];
     private Label lblStatus = new Label("X's turn to play");
     private Label lblIdentification = new Label();
 
     public void start(Stage primaryStage) {
-  
-	GridPane pane = new GridPane();
-	for (int i = 0; i < row_colnum; ++i)
-	    for (int j = 0; j < row_colnum; ++j) 
-		pane.add(cell[i][j] = new Cell(i,j), j, i);
-Button btn = new Button("OO");
-	BorderPane borderPane = new BorderPane();
-	borderPane.setCenter(pane);
+
+        GridPane pane = new GridPane();
+        for (int i = 0; i < row_colnum; ++i) {
+            for (int j = 0; j < row_colnum; ++j) {
+                pane.add(cell[i][j] = new Cell(i, j), j, i);
+            }
+        }
+        
+        Button btn = new Button("OO");
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(pane);
         borderPane.setLeft(btn);
-	borderPane.setTop(lblStatus);
-	borderPane.setBottom(lblIdentification);
-	
-	Scene scene = new Scene(borderPane, 750, 750);
-	primaryStage.setTitle("Nine Men's Morris");
-	primaryStage.setScene(scene);
-	primaryStage.show();
-	
-        
+        borderPane.setTop(lblStatus);
+        borderPane.setBottom(lblIdentification);
+
+        Scene scene = new Scene(borderPane, 750, 750);
+        primaryStage.setTitle("Nine Men's Morris");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
         btn.onMousePressedProperty(e -> {
-        
-	new Thread( () -> {
-		try {
-		    initializeRMI();
-		} catch (Exception ex) {
-		    ex.printStackTrace();
-		}
-	}).start();
-    });
+
+            new Thread(() -> {
+                try {
+                    initializeRMI();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }).start();
+        });
+
+    
 
     protected boolean initializeRMI() throws Exception {
-	String host = "";
-	try {
-            
-	} catch (Exception ex) {
-	    System.out.println(ex.getMessage());
-	}
+        String host = "";
+        try {
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
         return false;
 
     }
 
     public void setMyTurn(boolean myTurn) {
-	this.myTurn = myTurn;
+        this.myTurn = myTurn;
     }
 
     public void setMessage(String message) {
@@ -93,63 +96,63 @@ Button btn = new Button("OO");
     }
 
     public class Cell extends Pane {
-	private boolean marked = false;
-	int row, column;
-	private char token = ' ';
 
-	public Cell(final int row, final int column) {
-	    this.row = row;
-	    this.column = column;
-	    setStyle("-fx-border-color: black");
-	    this.setPrefSize(2000,2000);
-	    this.setOnMouseClicked(e -> handleMouseClick());
-	}
+        private boolean marked = false;
+        int row, column;
+        private char token = ' ';
 
-	public char getToken() {
-	    return token;
-	}
+        public Cell(final int row, final int column) {
+            this.row = row;
+            this.column = column;
+            setStyle("-fx-border-color: black");
+            this.setPrefSize(2000, 2000);
+            this.setOnMouseClicked(e -> handleMouseClick());
+        }
 
-	public void setToken(char c) {
-	    token = c;
-	    marked = true;
+        public char getToken() {
+            return token;
+        }
 
-	    if (token == 'X') {
-		Line line1 = new Line(10, 10, this.getWidth() - 10,
-				      this.getHeight() - 10);
-		line1.endXProperty().bind(this.widthProperty().subtract(10));
-		line1.endYProperty().bind(this.heightProperty().subtract(10));
+        public void setToken(char c) {
+            token = c;
+            marked = true;
 
-		Line line2 = new Line(10, this.getHeight() - 10,
-				      this.getWidth() - 10, 10);
-		line2.startYProperty().bind(this.heightProperty().subtract(10));
-		line2.endXProperty().bind(this.widthProperty().subtract(10));
+            if (token == 'X') {
+                Line line1 = new Line(10, 10, this.getWidth() - 10,
+                        this.getHeight() - 10);
+                line1.endXProperty().bind(this.widthProperty().subtract(10));
+                line1.endYProperty().bind(this.heightProperty().subtract(10));
 
-		Platform.runLater(() -> this.getChildren().addAll(line1, line2));
-	    } else if (token == 'O') {
-		Ellipse ellipse = new Ellipse(this.getWidth() / 2,
-					      this.getHeight() / 2,
-					      this.getWidth() / 2 - 10,
-					      this .getHeight() / 2 - 10);
-		ellipse.centerXProperty().bind(this.widthProperty().divide(2));
-		ellipse.centerYProperty().bind(this.heightProperty().divide(2));
-		ellipse.radiusXProperty().bind(this.widthProperty().divide(2).subtract(10));
-		ellipse.radiusYProperty().bind(this.heightProperty().divide(2).subtract(10));
-		ellipse.setStroke(Color.BLACK);
-		ellipse.setFill(Color.WHITE);
+                Line line2 = new Line(10, this.getHeight() - 10,
+                        this.getWidth() - 10, 10);
+                line2.startYProperty().bind(this.heightProperty().subtract(10));
+                line2.endXProperty().bind(this.widthProperty().subtract(10));
 
-		Platform.runLater(() -> getChildren().add(ellipse));
-	    }
-	}
+                Platform.runLater(() -> this.getChildren().addAll(line1, line2));
+            } else if (token == 'O') {
+                Ellipse ellipse = new Ellipse(this.getWidth() / 2,
+                        this.getHeight() / 2,
+                        this.getWidth() / 2 - 10,
+                        this.getHeight() / 2 - 10);
+                ellipse.centerXProperty().bind(this.widthProperty().divide(2));
+                ellipse.centerYProperty().bind(this.heightProperty().divide(2));
+                ellipse.radiusXProperty().bind(this.widthProperty().divide(2).subtract(10));
+                ellipse.radiusYProperty().bind(this.heightProperty().divide(2).subtract(10));
+                ellipse.setStroke(Color.BLACK);
+                ellipse.setFill(Color.WHITE);
 
-	private void handleMouseClick() {
-	    if (myTurn && !marked) {
-		setToken(marker);
-	    }
-	}
+                Platform.runLater(() -> getChildren().add(ellipse));
+            }
+        }
+
+        private void handleMouseClick() {
+            if (myTurn && !marked) {
+                setToken(marker);
+            }
+        }
     }
 
     public static void main(String[] args) {
-	launch(args);
+        launch(args);
     }
 }
-
