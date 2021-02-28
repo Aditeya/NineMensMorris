c2e8650cd3c99dd74c2f20febdfd9f9521950a58
aditeya <22963960+Aditeya@users.noreply.github.com>
2021-02-28 20:47:56 +0400

    Added input and output streams to NMMServiceThread

--- src/ninemensmorris/networking/NMMServiceThread.java
+++ src/ninemensmorris/networking/NMMServiceThread.java
@@ -5,6 +5,9 @@
  */
 package ninemensmorris.networking;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
@@ -36,40 +39,49 @@ public class NMMServiceThread extends Thread {
 
     @Override
     public void run() {
-        while (nmm.getMenLeft() > 0) {  //CHANGE TO WHILE MEN LEFT
-            PlayerTurn turn = nmm.getNmmTurn();
-            System.out.println(turn + " Player, Place a coin.");
-            System.out.println("match regex [A-H]+[1-3]");
-            String slot = "";
+        try {
+            ObjectInputStream p1ois = new ObjectInputStream(player1.getInputStream());
+            ObjectInputStream p2ois = new ObjectInputStream(player2.getInputStream());
+            ObjectOutputStream p1oos = new ObjectOutputStream(player1.getOutputStream());
+            ObjectOutputStream p2oos = new ObjectOutputStream(player2.getOutputStream());
             
-            switch (turn) {
-                case WHITE: //Play white turn
-                    break;
-                case BLACK: //Play black turn
-                    break;
-                default:    //Error
-                    break;
+            while (nmm.getMenLeft() > 0) {  //CHANGE TO WHILE MEN LEFT
+                PlayerTurn turn = nmm.getNmmTurn();
+                System.out.println(turn + " Player, Place a coin.");
+                System.out.println("match regex [A-H]+[1-3]");
+                
+                
+                NMMmove move;
+                switch (turn) {
+                    case WHITE: //Play white turn
+                        move = (NMMmove) p1ois.readObject();
+                        break;
+                    case BLACK: //Play black turn
+                        move = (NMMmove) p2ois.readObject();
+                        break;
+                    default:    //Error
+                        move = new NMMmove("A1");
+                        break;
+                }
+                
+                String slot = move.getMove();
+                NMMCoin coin = new NMMCoin(nmm.getNmmTurn().toMCntyp(), slot, null, null);
+
+                try {
+                    //Sets the coin
+                    sendCoin.put(coin);
+                    Thread.sleep(50);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(NMMLogicDemo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                //prints the board
+                nmm.cmdPrint(PrintType.VALUE);
             }
-
-            NMMCoin coin = new NMMCoin(nmm.getNmmTurn().toMCntyp(), slot, null, null);
-
-            try {
-                //Sets the coin
-                sendCoin.put(coin);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(NMMLogicDemo.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(NMMLogicDemo.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            //prints the board
-            nmm.cmdPrint(PrintType.VALUE);
-
+            
+            System.out.println("Game Complete");
+        } catch (IOException | ClassNotFoundException ex) {
         }
-        System.out.println("Game Complete");
     }
 
 }
---
+++ src/ninemensmorris/networking/NMMmove.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ninemensmorris.networking;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author aditeya
+ */
+public class NMMmove implements Serializable{
+    private final String move;
+
+    public NMMmove(String move) {
+        this.move = move;
+    }
+
+    public String getMove() {
+        return move;
+    }
+}
