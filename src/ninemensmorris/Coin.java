896b74c08de004c205adf0ce2109d67c07349a43
Lia4alNoor <47657455+Lia4alNoor@users.noreply.github.com>
2021-03-06 23:28:18 +0400

    Added OnDrag

--- src/ninemensmorris/Coin.java
+++ src/ninemensmorris/Coin.java
@@ -1,61 +1,75 @@
 /*
  */
 package nmmguisample;
+import java.sql.Types;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import static nmmguisample.SaMPLENMM.POS_SIZE;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Circle;
 
 /**
  */
-public class Coin extends StackPane {
+public class Coin extends Pane {
     private CoinType type;
     
     public CoinType getType(){
         return type;
     }
 
-    public Coin(CoinType type , int x, int y) {
+    public Coin (CoinType type , double x, double y) {
         this.type = type;
-        Ellipse bg = new Ellipse(POS_SIZE/15*3.125,POS_SIZE/15*3.125);
-        bg.setFill(Color.CORNFLOWERBLUE);
+        
+        Group CoinGroup = new Group();
+        
+         Circle bg = new Circle(POS_SIZE/15*3.125);
+        bg.setFill(Color.BLUE);
         bg.setStroke(Color.WHITESMOKE);
-        bg.setStrokeWidth(POS_SIZE*0.020);
-        bg.setTranslateX((POS_SIZE*3.125*2)/2) ;
-        System.out.println("Pos X "+(POS_SIZE*3.125*2)/2);
-        bg.setTranslateY((POS_SIZE*0.26*2)/2+POS_SIZE*0.07);
-        System.out.println("Pos Y "+((POS_SIZE*0.26*2)/2+POS_SIZE*0.07));
-
-//        bg.setCenterX(300);
-//        bg.setCenterY(300);
+        bg.setTranslateX(x);
+        bg.setTranslateY(y);
+       // bg.setStrokeWidth(POS_SIZE*0.020);
+      //  bg.setTranslateX((POS_SIZE*3.125*2)/2) ;
+      // bg.setTranslateY((POS_SIZE*0.26*2)/2+POS_SIZE*0.07);
         
         
         
-        Ellipse ellipse = new Ellipse(POS_SIZE/15*3.125,POS_SIZE/15*3.125);
-        ellipse.setFill(type== CoinType.WHITE ? Color.ANTIQUEWHITE :Color.BLUEVIOLET );
+        Circle ellipse = new Circle(POS_SIZE/15*3.125);
+        ellipse.setFill(type== CoinType.WHITE ? Color.WHITESMOKE :Color.BLUEVIOLET );
         ellipse.setStroke(Color.WHITESMOKE);
-        ellipse.setStrokeWidth(POS_SIZE*0.020);
-        ellipse.setTranslateX((POS_SIZE*3.125*2)/2) ;
-                System.out.println("Pos X "+(POS_SIZE*3.125*2)/2);
+         ellipse.setTranslateX(x-4);
+        ellipse.setTranslateY(y-4);
 
-       ellipse.setTranslateY((POS_SIZE*0.26*2)/2);
-               System.out.println("Pos X "+(POS_SIZE*0.26*2)/2);
-
-        ellipse.setCenterX(300);
-        ellipse.setCenterY(300);
-
-
+         System.out.println(" X "+x+" Y "+y);
+        //Sellipse.setStrokeWidth(POS_SIZE*0.020);
+        //ellipse.setTranslateX(x) ;
+       //ellipse.setTranslateY(y);
+       CoinGroup.getChildren().addAll(bg,ellipse);
+       CoinGroup.setOnMouseDragged(new EventHandler <MouseEvent>()
+        {
+            public void handle(MouseEvent event)
+            {
+                System.out.println("\"Event on Source: mouse dragged"+Types.TIME_WITH_TIMEZONE);
+                System.out.println("\"Event on Source: mouse draggedr34657");
+                event.setDragDetect(false);
+               
+            }
+        });
+       
+       CoinGroup.setOnMouseClicked(new EventHandler <MouseEvent>()
+        {
+            public void handle(MouseEvent event)
+            {
+                System.out.println("Clicked on");
+            }
+        });
         
-        Ellipse ellipse1 = new Ellipse(); {
-ellipse1.setCenterX(100);
-ellipse1.setCenterY(100);
-ellipse1.setRadiusX(10);
-ellipse1.setRadiusY(10);
-    }
-        ellipse1.setFill(Color.AQUAMARINE);
+        getChildren().addAll(CoinGroup);
         
         
-        getChildren().addAll(bg,ellipse);//,//ellipse1);
+       //,//ellipse1);
     }
     
     
