c2d3ba9bdd8dabc5c24e5aa713f5ac64d6d66520
EltoCode <ef356@live.mdx.ac.uk>
2021-03-27 23:33:12 +0400

    Updated Demo code to use latest NMMLogic

--- src/ninemensmorris/NMMLogicDemo.java
+++ src/ninemensmorris/NMMLogicDemo.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import ninemensmorris.enums.InputType;
 import ninemensmorris.enums.MCoinType;
 import ninemensmorris.enums.PrintType;
 
@@ -59,19 +60,20 @@ public class NMMLogicDemo {
         System.out.println(NMMLogic.slotLkUp("E3")[0]);
         System.out.println(NMMLogic.slotLkUp("E3")[1]);
         System.out.println("\n\n\n");
-        /*
+        
         //Use these to set coins
         //nmm.setNmmCnType(MCoinType.BLACK, slot) //will throw unsupportedException      
-        int[] numslot = NMMLogic.slotLkUp("A1");
+        /*int[] numslot = NMMLogic.slotLkUp("A1");
         nmm.setNmmCnType(MCoinType.WHITE, numslot[0], numslot[1]);
         numslot = NMMLogic.slotLkUp("G3");
         nmm.setNmmCnType(MCoinType.BLACK, numslot[0], numslot[1]);
-        nmm.cmdPrint(PrintType.VALUE);
-         */
-        /*
+        nmm.cmdPrint(PrintType.VALUE);*/
+         
+        
         //Required for setup
         LinkedBlockingQueue sendCoin = new LinkedBlockingQueue(2);
-
+        
+        /*
         //starts a new thread
         Thread setupTh = new Thread(new Runnable() {
 
@@ -84,16 +86,12 @@ public class NMMLogicDemo {
                     System.out.println("match regex [A-H]+[1-3]");
                     String slot = input.nextLine();
                     
-                    NMMCoin coin = new NMMCoin(nmm.getNmmTurn().toMCntyp(), slot, null, null);
+                    NMMCoin coin = new NMMCoin(nmm.getNmmTurn().toMCntyp(), slot, false, null, null);
 
                     try {
                         //Sets the coin
                         sendCoin.put(coin);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(NMMLogicDemo.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                    try {
+                        
                         Thread.sleep(50);
                     } catch (InterruptedException ex) {
                         Logger.getLogger(NMMLogicDemo.class.getName()).log(Level.SEVERE, null, ex);
@@ -102,17 +100,155 @@ public class NMMLogicDemo {
                     nmm.cmdPrint(PrintType.VALUE);
 
                 }
-                System.out.println("this fin");
+                System.out.println("setup fin");
+                
+                
             }
 
         });
 
         setupTh.start();
-
-        nmm.nmmSetup(sendCoin, true);
         */
         
-        NMMLogic.millLkUp("D3");
+        
+        Thread turnTh = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                
+                String slot = new String();
+                String slot2 = new String();
+                NMMCoin coin = null;
+                NMMCoin coin2 = null;
+
+                while(true)
+                {
+                    try
+                    {
+                        //prints the board
+                        nmm.cmdPrint(PrintType.VALUE); 
+                        
+                        switch(nmm.getInput())
+                        {
+                            case NONE:
+                                Thread.sleep(150); //optional
+                                //InputType woah = InputType.
+                                break;
+                                
+                            case PLACE:
+                                System.out.println(nmm.getNmmTurn() + " Player, Place a "
+                                        + "coin.");
+                                System.out.println("match regex [A-H]+[1-3]");
+                                slot = input.nextLine();
+                                if(!slot.matches("[A-H]+[1-3]")) //check for regex
+                                {
+                                    System.out.println("Does not match regex, "
+                                            + "please try again");
+                                    break;
+                                }
+                                
+                                coin = new NMMCoin(nmm.getNmmTurn().toMCntyp(), slot, false, null, null);
+
+                                //Sets the coin
+                                sendCoin.put(coin);
+                                Thread.sleep(50);
+                                break;
+                                
+                            case REMOVE:
+                                System.out.println(nmm.getNmmTurn() + " Player, "
+                                        + "Select an opposing coin to be removed");
+                                
+                                System.out.println("match regex [A-H]+[1-3]");
+                                slot = input.nextLine();
+                                if(!slot.matches("[A-H]+[1-3]")) //check for regex
+                                {
+                                    System.out.println("Does not match regex, "
+                                            + "please try again");
+                                    break;
+                                }
+                                
+                                coin = new NMMCoin(nmm.getNmmTurn().toMCntyp(), slot, false, null, null);
+
+                                //Sets the coin
+                                sendCoin.put(coin);
+                                Thread.sleep(50);
+                                break;
+                                
+                            case MOVE:
+                                System.out.println(nmm.getNmmTurn() + " Player, "
+                                        + "Select an coin to be moved");
+                                
+                                System.out.println("match regex [A-H]+[1-3]");
+                                slot = input.nextLine();
+                                if(!slot.matches("[A-H]+[1-3]")) //check for regex
+                                {
+                                    System.out.println("Does not match regex, "
+                                            + "please try again");
+                                    break;
+                                }
+                                
+                                System.out.println(nmm.getNmmTurn() + " Player, "
+                                        + "Move coin " + slot + " to?");
+                                
+                                System.out.println("match regex [A-H]+[1-3]");
+                                slot2 = input.nextLine();
+                                if(!slot.matches("[A-H]+[1-3]")) //check for regex
+                                {
+                                    System.out.println("Does not match regex, "
+                                            + "please try again");
+                                    break;
+                                }
+                                
+                                coin = new NMMCoin(nmm.getNmmTurn().toMCntyp(), slot, false, null, null);
+                                coin2 = new NMMCoin(nmm.getNmmTurn().toMCntyp(), slot2, false, null, null);
+
+                                //Sets the coin
+                                sendCoin.put(coin);
+                                sendCoin.put(coin2);
+                                Thread.sleep(50);
+                                break;
+                                
+                                
+                            default:
+                                System.out.println("oh snep something broke");
+                                System.exit(-1);
+                                
+                            
+                                
+                        }
+                    
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(NMMLogicDemo.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+
+                }
+             
+                
+            }
+
+        });        
+        
+        turnTh.start();
+
+        nmm.nmmSetup(sendCoin, true);
+
+        ////////////////////////
+        //////setup done////////
+        ////////////////////////
+        
+        ////////////////////////
+        /////////turns//////////
+        ////////////////////////
+        
+        
+        while(true)
+        {
+            nmm.nmmTurnHandle(sendCoin, true);
+        }
+       
+        
+        //NMMLogic.millLkUp("D3");
     }
 
     public static void test2(int[] test) {
