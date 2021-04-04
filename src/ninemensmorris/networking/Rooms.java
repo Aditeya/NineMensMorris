e8c37520860498d350341de3d4b0c45fb4898c19
aditeya <22963960+Aditeya@users.noreply.github.com>
2021-04-04 11:42:48 +0400

    Commented Rooms.java

--- src/ninemensmorris/networking/Rooms.java
+++ src/ninemensmorris/networking/Rooms.java
@@ -10,6 +10,8 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
+ * The Rooms class is used to interact with Rooms double array in a thread safe
+ * mannar.
  *
  * @author aditeya
  */
@@ -20,19 +22,28 @@ public class Rooms {
     private final Lock readLock;
     private final int[][] rooms;
 
+    /**
+     * Room Constructor which takes the number Maximum rooms.
+     * 
+     * @param rooms Maximum Number of Rooms
+     */
     public Rooms(int rooms) {
         this.lock = new ReentrantReadWriteLock();
         this.writeLock = lock.writeLock();
         this.readLock = lock.readLock();
 
         this.rooms = new int[rooms][2];
-
-//        for(int[] room: this.rooms) {
-//            room[1] = 0;
-//            room[2] = 0;
-//        }
     }
 
+    /**
+     * Acquires the lock, chooses the room and returns true if successful.
+     * If the selected room is not available then a false is returned 
+     * to signify failure.
+     * 
+     * @param ID    Client ID
+     * @param room  Room number of the room to be chosen.
+     * @return      returns if the operation was a success.
+     */
     public boolean chooseRoom(int ID, int room) {
         boolean success = false;
 
@@ -53,6 +64,11 @@ public class Rooms {
         return success;
     }
 
+    /**
+     * Aquires the lock and clears the room.
+     * 
+     * @param room Room number of the room to be cleared.
+     */
     public void clearRoom(int room) {
         try {
             writeLock.lock();
