9bd5fb9ac5bbb26ea3e485eec264fcec6400b6ff
aditeya <22963960+Aditeya@users.noreply.github.com>
2021-04-04 11:50:45 +0400

    Commented NCommand.java

--- src/ninemensmorris/networking/NCommand.java
+++ src/ninemensmorris/networking/NCommand.java
@@ -8,15 +8,17 @@ package ninemensmorris.networking;
 import java.io.Serializable;
 
 /**
- *
+ * NCommand Class is used to send commands, room numbers and room lists.
+ * 
  * @author aditeya
  */
-public class NCommand implements Serializable{
-    
+public class NCommand implements Serializable {
+
     private NetworkCommand command;
     private int room;
     private int[][] rooms;
 
+    //<editor-fold defaultstate="collapsed" desc="getters">
     public NetworkCommand getCommand() {
         return command;
     }
@@ -28,7 +30,9 @@ public class NCommand implements Serializable{
     public int[][] getRooms() {
         return rooms;
     }
+    //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="setters">
     public void setCommand(NetworkCommand command) {
         this.command = command;
     }
@@ -40,4 +44,5 @@ public class NCommand implements Serializable{
     public void setRooms(int[][] rooms) {
         this.rooms = rooms;
     }
+    //</editor-fold>
 }
