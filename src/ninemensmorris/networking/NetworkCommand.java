828d862a54f4aebf58f24d5eef338b243a3940b1
aditeya <22963960+Aditeya@users.noreply.github.com>
2021-04-04 11:58:43 +0400

    Commented NetworkCommand.java

--- src/ninemensmorris/networking/NetworkCommand.java
+++ src/ninemensmorris/networking/NetworkCommand.java
@@ -8,13 +8,14 @@ package ninemensmorris.networking;
 import java.io.Serializable;
 
 /**
+ * NetworkCommand Enum Class is for NetworkCommands 
  *
  * @author aditeya
  */
 public enum NetworkCommand implements Serializable{
-    LIST_ROOMS,
-    CHOOSE_ROOM,
-    ROOM_FULL,
-    ROOM_ACQ,
-    CONFIRM
+    LIST_ROOMS,     // List all the rooms
+    CHOOSE_ROOM,    // Choose a room
+    ROOM_FULL,      // The room is full
+    ROOM_ACQ,       // The room is acquired
+    CONFIRM         // Confirm client is ready
 }
