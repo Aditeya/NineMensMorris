03c36e15db6b9d4ceffcfbbbc0cae180121e67a4
EltoCode <ef356@live.mdx.ac.uk>
2021-04-05 19:51:05 +0400

    Fixed copyright stuff

--- src/ninemensmorris/NMMApplication.java
+++ src/ninemensmorris/NMMApplication.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2021 aditeya
+ * Copyright (C) 2021 elton
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author aditeya
+ * @author eltojaro
  */
 public class NMMApplication extends Application {
 
--- src/ninemensmorris/enums/InputType.java
+++ src/ninemensmorris/enums/InputType.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2021 aditeya
+ * Copyright (C) 2021 elton
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
