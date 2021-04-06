89d8d4eae342e0c93b42bc778b36da2dbcd99258
Lia4alNoor <47657455+Lia4alNoor@users.noreply.github.com>
2021-04-06 17:29:33 +0400

    All Not-Integrated Stuff +JSON ref inssue+ - jsoup.

--- src/ninemensmorris/NMMLogic.java
+++ src/ninemensmorris/NMMLogic.java
@@ -17,13 +17,9 @@
 package ninemensmorris;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.LinkedBlockingQueue;
